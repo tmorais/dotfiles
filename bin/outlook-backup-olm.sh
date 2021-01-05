02ce0a520c32685f7435f1b85b3362f2410a08e8
David Reis <dreis@amazon.com>
2021-01-05 15:21:27 -0300

    fixing scripts

--- bin/outlook-backup-olm.sh
+++ bin/outlook-backup-olm.sh
@@ -10,7 +10,7 @@ tell application "System Events"
         set {year:y, month:m, day:d} to (current date)
         set theMonthNumber to (m as integer)
         set theExportAsName to "outlook-backup-" & y & "-" & theMonthNumber & ".olm"
-        set theExportToFolder to "/Users/dreis/Desktop"
+        set theExportToFolder to "~/Desktop"
         --input the file name and the destination folder path you want to export as
         tell menu item "Export..." of menu "File" of menu bar item "File" of menu bar 1
             click
--- bin/s3-upsign.sh
+++ bin/s3-upsign.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 
-echo "$S3_UPSIGN_TEMP_BUCKET"
+if [ -z ${S3_UPSIGN_AWS_PROFILE+x} ]; then 
+    echo "EEE S3_UPSIGN_AWS_PROFILE is unset."; 
+    exit 2
+else 
+    echo "+++ S3_UPSIGN_AWS_PROFILE = '$S3_UPSIGN_AWS_PROFILE'"; 
+fi
 
 if [ -z ${S3_UPSIGN_TEMP_BUCKET+x} ]; then 
-    echo "var S3_UPSIGN_TEMP_BUCKET is unset. Please, include it on your .bash_profile or S3_UPSIGN_TEMP_BUCKET=\"s3://temp\" $0"; 
+    echo "EEE S3_UPSIGN_TEMP_BUCKET is unset. Please, include it on your .bash_profile or S3_UPSIGN_TEMP_BUCKET=\"s3://temp\" $0"; 
     exit 1;
 else 
-    echo "S3_UPSIGN_TEMP_BUCKET is set to '$S3_UPSIGN_TEMP_BUCKET'"; 
+    echo "+++ S3_UPSIGN_TEMP_BUCKET = '$S3_UPSIGN_TEMP_BUCKET'"; 
 fi
 
 if [ -z ${S3_UPSIGN_EXPIRES_IN+x} ]; then 
-    echo "var S3_UPSIGN_EXPIRES_IN is unset. Using one week as default."; 
+    echo "!!! S3_UPSIGN_EXPIRES_IN is unset. Using one week as default."; 
     S3_UPSIGN_EXPIRES_IN="3600*24*7"
 else 
-    echo "S3_UPSIGN_EXPIRES_IN is set to '$S3_UPSIGN_EXPIRES_IN'"; 
+    echo "+++ S3_UPSIGN_EXPIRES_IN = '$S3_UPSIGN_EXPIRES_IN'"; 
 fi
 
 FILENAME=$(basename $1)
 
-echo "S3 upsigning $1..."
-echo "Destination $S3_UPSIGN_TEMP_BUCKET/$FILENAME..."
+echo -e "\nS3 upsigning $1..."
+echo -e "\nDestination $S3_UPSIGN_TEMP_BUCKET/$FILENAME..."
 
-aws s3 cp "$1" "$S3_UPSIGN_TEMP_BUCKET"
+aws s3 cp "$1" "$S3_UPSIGN_TEMP_BUCKET" \
+    --acl private \
+    --profile "$S3_UPSIGN_AWS_PROFILE"
+
 if [ $? -ne 0 ]; then
-    echo "ERROR: Check your AWS PROFILE"
+    echo "EEE Check your AWS PROFILE"
     exit
 fi
 
 SIGNED_URL=`aws s3 presign "$S3_UPSIGN_TEMP_BUCKET/$FILENAME" \
-    --expires-in $(echo $S3_UPSIGN_EXPIRES_IN | bc -l)`
+    --expires-in $(echo $S3_UPSIGN_EXPIRES_IN | bc -l) \
+    --profile "$S3_UPSIGN_AWS_PROFILE"`
 
 echo -e "S3 signed URL...\n\n$SIGNED_URL\n\n"
